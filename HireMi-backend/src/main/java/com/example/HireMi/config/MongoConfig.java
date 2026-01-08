f79c7ff04d413f0a06f560484026f7bb79db1a0d
anupkoshti <thdarklord12345@gmail.com>
2026-01-09 02:49:09 +0530

    fixed mongodb url string pushed to github

--- HireMi-backend/src/main/java/com/example/HireMi/config/MongoConfig.java
+++ HireMi-backend/src/main/java/com/example/HireMi/config/MongoConfig.java
@@ -5,24 +5,28 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
-public class MongoConfig extends AbstractMongoClientConfiguration {
+public class MongoConfig {
+
+    @Value("${MONGODB_URI}")
+    private String mongoUri;
 
     @Value("${spring.data.mongodb.database:clearhire}")
     private String databaseName;
 
-    @Override
-    protected String getDatabaseName() {
-        return databaseName;
+    @Bean
+    @Primary
+    public MongoClient mongoClient() {
+        System.out.println("🔗 Connecting to MongoDB Atlas: " + mongoUri.replaceAll(":[^@]*@", ":***@"));
+        return MongoClients.create(mongoUri);
     }
 
     @Bean
-    @Override
-    public MongoClient mongoClient() {
-        String connectionString = "mongodb+srv://thdarklord12345_db_user:8mcZ329BgAm30p81@cluster0.wcwqpgi.mongodb.net/clearhire?retryWrites=true&w=majority&appName=Cluster0";
-        System.out.println("🔗 Connecting to MongoDB Atlas: " + connectionString.replaceAll(":[^@]*@", ":***@"));
-        return MongoClients.create(connectionString);
+    @Primary
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), databaseName);
     }
 }
